d3c78b051163d01755678f2e844d6f2117cde443
Mike Smullin <mike@smullindesign.com>
2013-04-11 23:36:13 -0600

    compatibility with Chef 11

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,7 @@ description      "Installs/Configures samba to use Active Directory via Kerberos
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
 
+depends "samba"
 recipe "samba::default", "Includes the samba::client recipe"
 recipe "samba::client", "Installs smbclient package"
 recipe "samba::server", "Installs samba server packages and configures smb.conf"
